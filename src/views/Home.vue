a2409166a5e1aea44bb6a1dc39d2ebd08647e41a
Chad Smith <smith.chad12@gmail.com>
2019-09-11 00:50:13 -0500

    add basic modal functionality with example

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,41 @@
 <template>
   <div class="home">
     <img alt="Vue logo" src="../assets/logo.png" />
+    <button type="button" @click="openModal">Open Modal</button>
     <HelloWorld msg="Welcome to Your Vue.js + TypeScript App" />
+
+    <AppModal
+      title="Hello  World"
+      v-model="isModalOpen"
+      :stay-open="true"
+      @save="saveForm"
+    >
+      This is the default modal!
+    </AppModal>
   </div>
 </template>
 
 <script lang="ts">
 import { Component, Vue } from "vue-property-decorator";
 import HelloWorld from "@/components/HelloWorld.vue"; // @ is an alias to /src
+import AppModal from "@/components/modal/AppModal.vue";
 
 @Component({
   components: {
-    HelloWorld
+    HelloWorld,
+    AppModal
   }
 })
-export default class Home extends Vue {}
+export default class Home extends Vue {
+  private isModalOpen: boolean = false;
+
+  private openModal() {
+    this.isModalOpen = true;
+  }
+  private saveForm() {
+    setTimeout(() => {
+      this.isModalOpen = false;
+    }, 2000);
+  }
+}
 </script>
